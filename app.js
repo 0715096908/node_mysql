edf93ee2d433b96243cdfd41eb3eb8d3b71bd2b9
Tony Okoth <blairtony2014@gmail.com>
2018-09-22 15:57:01 +0300

    Make changes

--- app.js
+++ app.js
@@ -3,6 +3,28 @@ const mysql = require('mysql');
 
 const app = express();
 
-app.listen(3000, () => { conslole.log('Server started on port 3000');
+//create Database
+app.get('/createdb', (req,res) => {
+    let sql = 'CREATE DATABASE nodemysql';
+    db.query(sql,(err,result)=>{
+        if(err)
+        {
+            throw err;
+        }
+        console.log(result);
+        res.send('Database Created Successfully');
+    })
 });
+
+//create a connection variable
+const db = mysql.createConnection({host: 'localhost', user : 'root', password : '', database : 'nodemysql'});
+db.connect((err)=> {
+    if(err)
+    {
+       throw err;
+    }
+    console.log('Mysql connected');
+});
+app.listen('3000', () => { console.log('Server started on port 3000');
+});  
 
